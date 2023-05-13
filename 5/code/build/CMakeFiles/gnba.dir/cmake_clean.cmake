533bb6f1f175e11f42aa38a1ee7e4cce302a22e5
Tyler <2967035281@qq.com>
2023-05-13 18:24:14 +0800

    1

---
+++ 5/code/build/CMakeFiles/gnba.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gnba.dir/GN-BA.cpp.o"
+  "gnba"
+  "gnba.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gnba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
